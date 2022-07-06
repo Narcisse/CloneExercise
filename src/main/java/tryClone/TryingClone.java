72ae00a349364a062a9711572a4cd1544845d376
Narcisse <106417952+Narcisse@users.noreply.github.com>
2022-07-06 11:41:28 -0500

    NasCommit

---
+++ src/main/java/tryClone/TryingClone.java
@@ -0,0 +1,41 @@
+package tryClone;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TryingClone {
+
+	WebDriver driver;
+	
+	@Before
+	public void init() {
+		
+		System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("https://techfios.com/billing/?ng=admin/");
+	}
+	
+@Test
+	public void loginTest() {
+		
+		driver.findElement(By.id("username")).sendKeys("demo@techfios.com");
+		driver.findElement(By.id("password")).sendKeys("abc123");
+		driver.findElement(By.name("login")).click();
+		
+	}
+
+	@After
+	public void tearDown() {		
+		driver.close();
+		driver.quit();
+	}
+
+}
